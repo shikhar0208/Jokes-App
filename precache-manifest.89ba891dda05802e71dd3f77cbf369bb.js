75bc172efc68ab48b4c0220f7002ab5af8327b94
Shikhar Rastogi <shikharrastogi.0208@gmail.com>
2020-04-10 03:06:37 +0530

    Updates

---
+++ precache-manifest.89ba891dda05802e71dd3f77cbf369bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33796cc3ce53ea9edc5896fbb885b4e5",
+    "url": "/Jokes-App/index.html"
+  },
+  {
+    "revision": "a1f94f3c42154ced0df3",
+    "url": "/Jokes-App/static/css/main.c4e335bb.chunk.css"
+  },
+  {
+    "revision": "4fd1249919f299986583",
+    "url": "/Jokes-App/static/js/2.9c5222f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jokes-App/static/js/2.9c5222f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f94f3c42154ced0df3",
+    "url": "/Jokes-App/static/js/main.f8b1616c.chunk.js"
+  },
+  {
+    "revision": "8dcafdf42c60fce0fa6b",
+    "url": "/Jokes-App/static/js/runtime-main.d52f0ca4.js"
+  }
+]);
